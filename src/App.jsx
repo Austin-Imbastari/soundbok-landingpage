a7590b52046b2fdc8271ab2b87c598ba38da49d9
austin Imbastari <austinimbastari@gmail.com>
2024-03-08 00:05:07 -0800

    boiler plate created

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,7 @@ function App() {
                 gl={{
                     antialias: true,
                 }}
-                camera={{
-                    fov: 45,
-                    near: 0.1,
-                    far: 200,
-                    position: [3, 2, 6],
-                }}
+                camera={{}}
             >
                 <Experience />
             </Canvas>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,43 +1,27 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unknown-property */
 import React, { useRef } from "react";
-import { OrbitControls, TransformControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 
 const Experience = () => {
-    const boxRef = useRef();
-    const groupRef = useRef();
+    const bokRef = useRef();
 
     useFrame((state, delta) => {
-        boxRef.current.rotation.y += delta;
-
-        // const angle = state.clock.elapsedTime;
-        // state.camera.position.x = Math.sin(angle) * 8;
-        // state.camera.position.z = Math.cos(angle) * 8;
-        // state.camera.lookAt(0, 0, 0);
+        // const angle = state.clock.getElapsedTime();
+        // bokRef.current.position.x += Math.sin(angle) * delta;
+        // bokRef.current.position.z += Math.cos(angle) * delta;
     });
 
     return (
         <>
-            {/* <OrbitControls /> */}
+            <OrbitControls />
             <directionalLight position={[1, 2, 3]} intensity={4.5} />
             <ambientLight intensity={1.5} />
 
-            <group ref={groupRef}>
-                <TransformControls>
-                    <mesh>
-                        <sphereGeometry args={[1.5, 32, 32]} />
-                        <meshStandardMaterial color='mediumpurple' />
-                    </mesh>
-                </TransformControls>
-                <mesh ref={boxRef} position={[-3, 0, 1]}>
-                    <boxGeometry />
-                    <meshStandardMaterial color='orange' />
-                </mesh>
-            </group>
-            <mesh rotation-x={-Math.PI * 0.5} position={[0, -1.5, 0]} scale={20}>
-                <planeGeometry />
-                <meshStandardMaterial color='green' />
+            <mesh ref={bokRef}>
+                <boxGeometry />
+                <meshStandardMaterial color='yellow' />
             </mesh>
         </>
     );
